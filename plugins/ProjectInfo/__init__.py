0d2e5e4be7b662225424506df1726c2cf7f950f7
Toni <toni.schoenbuchner@csgis.de>
2025-03-13 11:35:39 +0100

    sftp user

--- plugins/ProjectInfo/__init__.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-QGIS Server Plugin: ProjectInfo
-Provides JSON endpoint for QGIS Server project information
-"""
-
-# Import the ServerClassFactory function directly from the implementation file
-from .projectinfo import serverClassFactory
